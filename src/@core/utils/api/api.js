8ffc651e518788accd8a861125e86693093e5240
18521184 <80056369+18521184@users.noreply.github.com>
2022-05-29 14:26:36 +0700

    change search api

--- src/@core/utils/api/api.js
+++ src/@core/utils/api/api.js
@@ -1,6 +1,7 @@
 import { SearchClient } from '@core/proto/serve_grpc_web_pb';
 import { Data } from '@core/proto/serve_pb';
 import { formatDataSearch } from '@core/utils/modules/modules';
+import axios from 'axios';
 
 const metadata = { 'custom-header-1': 'SearchResult' };
 
@@ -17,19 +18,23 @@ export const search = async (text, number) => {
         }
         var data = [];
         var trackingData = [];
-        trackingData.push({percent: result.array[0][1]});
+        trackingData.push({ percent: result.array[0][1] });
         if (result.array[0][2] === undefined) {
-          trackingData.push({desition: false});
-        }else {
-          trackingData.push({desition: result.array[0][2]});
+          trackingData.push({ desition: false });
+        } else {
+          trackingData.push({ desition: result.array[0][2] });
         }
         var contextData = formatDataSearch(result.array[0][0]);
-        data.push(trackingData,contextData);
-        data.push({sentence: text});
+        data.push(trackingData, contextData);
+        data.push({ sentence: text });
         return resolve(data);
       });
     } catch (error) {
       reject(error);
     }
   });
+};
+
+export const searchAmp = async (input) => {
+  return await axios.post('http://10.71.11.71:8888/api/search/answering/', input, { 'Content-Type': 'multipart/form-data' });
 };
--- src/page/search/search.js
+++ src/page/search/search.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './search.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { search } from '@core/utils/api/api';
+// import { search } from '@core/utils/api/api';
+import { searchAmp } from '@core/utils/api/api';
 import { Space40, Space60, Space120 } from '@core/components/atom/space/space';
 import { formatString } from '@core/utils/modules/modules';
 import { Spinner } from 'react-bootstrap';
@@ -12,7 +13,7 @@ export default class Search extends React.Component {
     this.state = {
       text: '',
       number: 4,
-      data: [],
+      data: '',
       percent: 0,
       tracking: false,
       isSearch: false,
@@ -21,7 +22,7 @@ export default class Search extends React.Component {
     };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     AOS.init({
       once: true
     });
@@ -29,31 +30,22 @@ export default class Search extends React.Component {
   }
 
   render() {
-    const { text, number, data, isSearch, disable, error } = this.state;
+    const { text, data, isSearch, error } = this.state;
 
-    const resultSearch = (data) => {
-      console.log(data);
-      if(data.length === 0) {
-        return(
-          <div></div>
-        );
-      }
-      else if (data.length > 0) {
-        return (
-          <div className='search__result'>
-            <div className='result'>
-              <h5>Rate the search sentence</h5>
-              <p>With your sentences: <span className='sentence--modifile'> {data[2].sentence} </span></p>
-              <p>We have accuracy: <span>{(Number(data[0][0].percent)*100).toFixed(5)+'%'}</span></p>
-              <p>
-                - With the above search and accuracy we conclude this sentence: <span className={data[0][1].desition ? 'txt-green' : 'txt-red'}>{ data[0][1].desition.toString() }</span>
-              </p>
-            </div>
-          </div>
-        );
-      }
-      return 0;
-    };
+    // const resultSearch = (data) => {
+    //   console.log(data);
+    //   if (data.answer.length === 0) {
+    //     return (
+    //       <div></div>
+    //     );
+    //   }
+    //   else if (data.length > 0) {
+    //     return (
+          
+    //     );
+    //   }
+    //   return 0;
+    // };
 
     const listItem = (data) => {
       if (data.length === 0) {
@@ -76,7 +68,7 @@ export default class Search extends React.Component {
               </p>
               <p>Description: {formatString(item['description'])}</p>
               <div className='content__btn-link'>
-                <a className='btn-link--modifile' href={ `${item['link']}` }>Read more</a>
+                <a className='btn-link--modifile' href={`${item['link']}`}>Read more</a>
               </div>
             </div>
           </div>
@@ -106,8 +98,8 @@ export default class Search extends React.Component {
           data-aos-duration='600'
           className='search__description'>
           <p className='description__layout'>
-          Verify statistical claims about the coronavirus spread and effects on
-          data from the <span className='description--modifile'>official sources</span>.
+            Verify statistical claims about the coronavirus spread and effects on
+            data from the <span className='description--modifile'>official sources</span>.
           </p>
         </div>
         <Space40></Space40>
@@ -116,7 +108,7 @@ export default class Search extends React.Component {
           data-aos-easing='ease-in-sine'
           data-aos-duration='600'
           data-aos-delay='600'
-          className= 'search'>
+          className='search'>
           <div className='search__layout'>
             <input
               className='input-search'
@@ -124,7 +116,7 @@ export default class Search extends React.Component {
               placeholder='Sentence about covid-19'
               onChange={async (event) => {
                 await this.setState({ text: event.target.value });
-                if( text.length > 3 ) {
+                if (text.length > 0) {
                   await this.setState({ disable: false });
                 } else {
                   this.setState({ disable: true });
@@ -132,31 +124,46 @@ export default class Search extends React.Component {
               }}
             />
             <button
-              className= 'btn-search'
-              disabled = { disable }
-              onClick={async() => {
-                if( text.length === 0 ) {
-                  this.setState({error: true});
+              className='btn-search'
+              // disabled={disable}
+              onClick={async () => {
+
+                if (text.length === 0) {
+                  this.setState({ error: true });
                   return;
                 }
                 this.setState({ isSearch: true });
-                await search(text, number).then(async res => {
-                  if(!res) {
-                    this.setState({ error: true });
-                    this.setState({ isSearch: false });
-                    return;
-                  }
-                  await this.setState({ data: res});
-                  await this.setState({ isSearch: false });
-                  await this.setState({ disable: true });
-                  await this.setState({ text: '' });
-                  document.getElementById('input-search').value = '';
-                });
+                let bodyFormData = new FormData();
+                bodyFormData.append('data', text);
+                const res = await searchAmp(bodyFormData);
+                console.log(res);
+                if (!res.data.answer) {
+                  this.setState({ error: true });
+                  this.setState({ isSearch: false });
+                  return;
+                }
+                await this.setState({ data: res.data });
+                await this.setState({ isSearch: false });
+                await this.setState({ disable: true });
+                await this.setState({ text: '' });
+                document.getElementById('input-search').value = '';
+                // await search(text, number).then(async res => {
+                //   if(!res) {
+                //     this.setState({ error: true });
+                //     this.setState({ isSearch: false });
+                //     return;
+                //   }
+                //   await this.setState({ data: res});
+                //   await this.setState({ isSearch: false });
+                //   await this.setState({ disable: true });
+                //   await this.setState({ text: '' });
+                //   document.getElementById('input-search').value = '';
+                // });
               }}
             >
               <div className='btn-search__layout'>
                 <div className='txt-search'>Tracking</div>
-                {isSearch?
+                {isSearch ?
                   <Spinner animation='border' role='status' size='sm' variant='light' className='spinner-custom'>
                     <span className='visually-hidden'>Loading...</span>
                   </Spinner>
@@ -166,9 +173,20 @@ export default class Search extends React.Component {
               </div>
             </button>
           </div>
-          {error? <div className='search__error'><i className='bx bx-error'></i> Request server error. Please try again!</div> : <div style={{marginTop:'16px'}}></div>}
+          {error ? <div className='search__error'><i className='bx bx-error'></i> Request server error. Please try again!</div> : <div style={{ marginTop: '16px' }}></div>}
         </div>
-        {resultSearch(data)}
+        {data.answer && <div className='search__result'>
+          <div className='result'>
+            <h5>Rate the search sentence</h5>
+            <p>With your sentences: <span className='sentence--modifile'> {data.query} </span></p>
+            <p>
+              - With the above search and accuracy we conclude this sentence: <span className={data.answer ? 'txt-green' : 'txt-red'}>{data.answer}</span>
+            </p>
+            <p>
+              - Context: {data.document.content}
+            </p>
+          </div>
+        </div>}
         <Space60></Space60>
         <div
           data-aos='fade-up'
